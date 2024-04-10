7d59a2b621b64a48031c63d638107555d8259a94
Luis Felipe Trindade <felipe.zucuni@gmail.com>
2024-04-09 21:39:27 -0300

    Versão com todas correção para M6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have the 'admin-lte' title`, () => {
+	it(`should have the 'Engenharia de Software - M6' title`, () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
-		expect(app.title).toEqual('Engenharia de Software M6');
+		expect(app.title).toEqual('Engenharia de Software - M6');
 	});
 
 	it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
 	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-	title = 'Engenharia de Software M6';
+	title = 'Engenharia de Software - M6';
 }
